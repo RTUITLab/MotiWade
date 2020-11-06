f319ee8df7a7060be9470966133477415e128bb9
Maksim Makushchenko <maksalmak@gmail.com>
2020-11-06 22:11:57 +0300

    Improve migration script

--- RealityShiftLearning/addMigration.ps1
+++ RealityShiftLearning/addMigration.ps1
@@ -1,4 +1,2 @@
-$Env:DbType='SQLite'
-dotnet ef migrations add -p ../Database.SQLite "$($args[0])_SQLite"
-$Env:DbType='Postgres'
-dotnet ef migrations add -p ../Database.Postgres "$($args[0])_Postgres"
+$Env:DbType='SQLite'; dotnet ef migrations add -p ../Database.SQLite "$($args[0])_SQLite"
+$Env:DbType='Postgres'; dotnet ef migrations add -p ../Database.Postgres "$($args[0])_Postgres"
