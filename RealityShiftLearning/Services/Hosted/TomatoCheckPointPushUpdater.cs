1e765c4508ac17f08f74631634884eb53ea7e3fb
Maksim Makushchenko <maksalmak@gmail.com>
2020-11-08 09:28:22 +0300

    Add some logs

--- RealityShiftLearning/Services/Hosted/TomatoCheckPointPushUpdater.cs
+++ RealityShiftLearning/Services/Hosted/TomatoCheckPointPushUpdater.cs
@@ -1,7 +1,9 @@
 ﻿using Database;
+using Google.Apis.Logging;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Models;
 using System;
 using System.Collections.Concurrent;
@@ -14,10 +16,14 @@ namespace RealityShiftLearning.Services.Hosted
 {
     public class TomatoCheckPointPushUpdater : BackgroundService
     {
+        private readonly ILogger<TomatoCheckPointPushUpdater> logger;
         private readonly IServiceScopeFactory scopeFactory;
         private ConcurrentDictionary<int, TomatoTimer.TimerState> progresses = new ConcurrentDictionary<int, TomatoTimer.TimerState>();
-        public TomatoCheckPointPushUpdater(IServiceScopeFactory scopeFactory)
+        public TomatoCheckPointPushUpdater(
+            ILogger<TomatoCheckPointPushUpdater> logger,
+            IServiceScopeFactory scopeFactory)
         {
+            this.logger = logger;
             this.scopeFactory = scopeFactory;
         }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -25,6 +31,7 @@ namespace RealityShiftLearning.Services.Hosted
             while (!stoppingToken.IsCancellationRequested)
             {
                 await Task.Delay(TimeSpan.FromSeconds(3));
+                logger.LogDebug("Start iteration");
                 using var scope = scopeFactory.CreateScope();
                 var db = scope.ServiceProvider.GetRequiredService<LearnDbContext>();
                 var pushService = scope.ServiceProvider.GetRequiredService<PushNotifyService>();
@@ -33,6 +40,7 @@ namespace RealityShiftLearning.Services.Hosted
                     .Include(ute => ute.TomatoTimer)
                     .Include(ute => ute.Exercise)
                     .ToListAsync();
+                logger.LogDebug($"Receive {links.Count} records");
                 foreach (var link in links)
                 {
                     bool needUpdate = false;
@@ -44,6 +52,7 @@ namespace RealityShiftLearning.Services.Hosted
                     });
                     if (needUpdate)
                     {
+                        logger.LogInformation($"send notify about exercise {link.ExerciseId}");
                         if (tomatoState.State == TomatoTimer.TimerState.Free)
                         {
                             await pushService.SendNotifyToUser(
